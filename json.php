5a45902d86e96d5a8c96d126c3cf0d1abfceaea1
LarisaGl <89888985265@mail.ru>
2018-05-25 15:11:53 +0300

    new

--- json.php
+++ json.php
@@ -1,48 +1,64 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Погода</title>
+</head>
+<body>
+
 <?php
     $quest='http://api.openweathermap.org/data/2.5/weather?q=Rostov-na-Donu,ru&appid=f3d4855cbacbc20a8845e49073b854df';
     $resp=file_get_contents($quest);
+    if ($resp === false) {
+    	die("Не удалось получить данные");
+    }
     $json=json_decode($resp, true);
+    if ($json === null) {
+    	die("Ошибка декодирования json"); }
 ?>
 
 <table>
 	<tr>		
-		<td><img src="http://www.iconsearch.ru/uploads/icons/aquanox/128x128/globe.png"></td>
+		<td><img src="img/globe.png"></td>
 		<td><b><?php echo "City name:";?></b></td>
-		<td><?php echo $json['name'];?></td>
+		<td><?php if (!empty($json['name'])) { echo $json['name']; } else { echo 'Не удалось получить название города'; } ?></td>
 	</tr>
 	<tr>
-		<td><img src="http://remont-kraski.ru/images/services/services4/remont6/2.png"></td>		
+		<td><img src="img/2.png"></td>		
 		<td><b><?php echo "Temperature:";?></b></td>
-		<td><?php echo $json['main']['temp'];?></td>
+		<td><?php if (!empty($json['main']['temp'])) { echo $json['main']['temp']; } else { echo 'Не удалось получить данные температуры'; } ?></td>
 	</tr>
 	<tr>
-		<td><img src="http://icdn.pro/images/es/t/i/tiempo-sol-nube-icono-9235-128.png"></td>		
+		<td><img src="img/tiempo-sol-nube-icono-9235-128.png"></td>		
 		<td><b><?php echo "Today:";?></b></td>
-		<td><?php echo $json['weather']['0']['main'];?></td>
+		<td><?php if (!empty($json['weather']['0']['main'])) { echo $json['weather']['0']['main']; } else { echo 'Не удалось получить данные погоды'; } ?></td>
 	</tr>
 	<tr>
-		<td><img src="https://images.freeimages.com/images/premium/large-thumbs/2622/26221249-water-drop.jpg"></td>
+		<td><img src="img/26221249-water-drop.jpg"></td>
 		<td><b><?php echo "Humidity:";?></b></td>
-		<td><?php echo $json['main']['humidity'];?></td>
+		<td><?php if (!empty($json['main']['humidity'])) { echo $json['main']['humidity']; } else { echo 'Не удалось получить данные погоды'; } ?></td>
 	</tr>
 	<tr>
-		<td><img src="http://www.lotusite.ru/images/userfiles/images/i(33).jpeg"></td>
+		<td><img src="img/i(33).jpeg"></td>
 		<td><b><?php echo "Minimum temperature at the moment:";?></b></td>
-		<td><?php echo $json['main']['temp_min'];?></td>
+		<td><?php if (!empty($json['main']['temp_min'])) { echo $json['main']['temp_min']; } else { echo 'Не удалось получить данные температуры'; } ?></td>
 	</tr>
 	<tr>
-		<td><img src="http://www.lotusite.ru/images/userfiles/images/i(33).jpeg"></td>
+		<td><img src="img/i(33).jpeg"></td>
 		<td><b><?php echo "Maximum temperature at the moment:";?></b></td>
-		<td><?php echo $json['main']['temp_max'];?></td>
+		<td><?php if (!empty($json['main']['temp_max'])) { echo $json['main']['temp_max']; } else { echo 'Не удалось получить данные температуры'; } ?></td>
 	</tr>	
 	<tr>
-		<td><img src="https://st2.depositphotos.com/3557671/9239/v/170/depositphotos_92394144-stock-illustration-wind-generator-icon-on-white.jpg"></td>	
+		<td><img src="img/depositphotos_92394144-stock-illustration-wind-generator-icon-on-white.jpg"></td>	
 		<td><b><?php echo "Wind speed";?></b></td>
-		<td><?php echo $json['wind']['speed'];?></td>
+		<td><?php if (!empty($json['wind']['speed'])) { echo $json['wind']['speed']; } else { echo 'Не удалось получить данные о скорости ветра'; } ?></td>
 	</tr>
 	<tr>
-		<td><img src="http://s1.iconbird.com/ico/0612/VistaStyleWeatherIconsSet/w256h2561339359708WindFlagStorm.png"></td>
+		<td><img src="img/w256h2561339359708WindFlagStorm.png"></td>
 		<td><b><?php echo "Wind direction";?></b></td>
-		<td><?php echo $json['wind']['deg'];?></td>
+		<td><?php if (!empty($json['wind']['deg'])) { echo $json['wind']['deg']; } else { echo 'Не удалось получить данные о направлении ветра'; } ?></td>
 	</tr>
 </table>
+
+</body>
+</html>
